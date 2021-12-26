e513ad55ff096f51cfbd0a87273f8b6f3405a19b
Vitaliy Ippolitov <tobestmail@gmail.com>
2021-12-26 21:04:56 +0300

    [writtingThePost] Fix warnings && topic name

--- src/main/kotlin/com/vito/spring/examples/kafka/streams/service/MessageServiceImpl.kt
+++ src/main/kotlin/com/vito/spring/examples/kafka/streams/service/MessageServiceImpl.kt
@@ -2,13 +2,14 @@ package com.vito.spring.examples.kafka.streams.service
 
 import com.vito.spring.examples.kafka.streams.logger
 import com.vito.spring.examples.kafka.streams.producer.ProducerBinding
+import org.slf4j.Logger
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Service
 
 @Service
 class MessageServiceImpl(val producerBinding: ProducerBinding) : MessageService {
 
-    val log by logger()
+    val log: Logger by logger()
 
     override fun consume(message: Map<String, Any?>) {
         val msg = MessageBuilder.withPayload(message).build()
